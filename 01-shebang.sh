f6cfc704453df0d2714a6cdb53b31b1789552ec2
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-14 23:15:40 -0600

    Shebang Topics

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -3,3 +3,5 @@
 # #! is called as she-bang
 # Completely optional
 # adding that will have an understanding of what script it is
+
+#shell scripting is nothing but adding all your manual commands in a particular order
