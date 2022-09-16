317d80f23ef046235adfebc0deb427c4cb0c6a73
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-16 16:02:44 -0600

    conditions

--- 06-if.sh
+++ 06-if.sh
@@ -7,3 +7,16 @@ else
   echo $a is lesser than 10
 fi
 
+
+x="abc"
+if [ $x == "abc" ]; then
+  echo Yes both are equal
+fi
+
+
+file=/tmp/new
+if [ -f $file ]; then
+  echo File Exists
+  else
+    echo File does not exit
+fi
